9ac429bd338e1636620df285ec0d2bac5759a228
kabram <kabram@ROSECND70852HX.ES.AD.ADP.com>
2017-06-21 16:13:33 -0400

    Initial Push

---
+++ EPMS/src/main/java/com/adp/hoc/epms/EpmsApplication.java
@@ -0,0 +1,12 @@
+package com.adp.hoc.epms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpmsApplication.class, args);
+	}
+}
---
+++ EPMS/src/test/java/com/adp/hoc/epms/EpmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.adp.hoc.epms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EpmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
