72654d87e2a1d9eca3870039403992614bc1c233
kabram <kabram@ROSECND70852HX.ES.AD.ADP.com>
2017-06-22 17:05:49 -0400

    Committing the first controller

---
+++ EPMS/src/main/java/com/adp/hoc/epms/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.adp.hoc.epms.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="test/")
+public class TestController {
+	
+	@RequestMapping(value="test")
+	public String test(){
+		return "successfull";
+	}
+
+}
